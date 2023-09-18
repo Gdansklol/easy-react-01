ad5875743a737bd2302a6eead52c0b777f5c06f6
swedenebomi <swedenebomi@gamil.com>
2023-09-18 17:39:46 +0200

    Working test, and count useEffect

--- src/App.js
+++ src/App.js
@@ -6,16 +6,25 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+ 
+ useEffect (()=>{
+  console.log(count)
+ })
 
   const increment = () => {
     setCount(count + 1);
   };
 
+  const decrement = () => {
+    setCount(count -1)
+  }
+ console.log('ok rendering')
   return (
-    <div className='App'>
-      <h1> Cruella Lee </h1>
-      <button onClick={increment}>Click {count}</button>
-    </div>
+  <div className='App' >
+    <h1>Code with useEffect</h1>
+    <button onClick={increment}>+ Increment button</button>
+    <button onClick={decrement}>- Decrement button</button>
+  </div>
   );
 };
 
