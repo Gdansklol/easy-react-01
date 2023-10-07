4538aa4c278564ddcf56ed2ab422f30183cf6401
swedenebomi <swedenebomi@gamil.com>
2023-10-08 01:23:53 +0200

    07

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // 07 Eliminate repetition with components
 
 import React, { useEffect, useState } from 'react';
@@ -30,74 +29,3 @@ function App() {
 };
 
 export default App
-=======
-// 05- useState, useEffect
-
-import React,{useState,useEffect} from 'react';
-
-function App() {
-  const [count, setCount] =useState(0);
-  const [cruella, setCruella] =useState(0);
-  useEffect (()=>{
-    console.log(count,cruella)
-  },[count,cruella])
-
-  useEffect (()=> {
-    console.log('first rendreing with empty array []')
-  },[])
-
-  const increment =() => {
-    setCount(count +1)
-  }
-
-  return (
-    <div className='App'>
-      <h1>Cruella coder</h1>
-      <button onClick={increment}>Click for increment</button>
-      <button onClick={()=> setCruella()}>Click 2</button>
-    </div>
-  );
-};
-
-export default App;
-
-
-
-
-
-
-
-
-// // 05 - useEffect
-// import React, {useEffect, useState} from 'react';
-// import './App.css';
-
-// function App() {
-//   const [count, setCount] = useState(0);
-//   const [test, setTest] = useState(0)
- 
-//  useEffect (()=>{
-//   console.log(count + ", " + test)
-//  },[count])
-
-//  useEffect (()=> {
-//   console.log('first rendring and no more rendring')
-//  },[])
-//   const increment = () => {
-//     setCount(count +1);
-//   }
-
-//   const decrement = () => {
-//     setTest (test -1);
-//   }
-//  console.log('rendering works')
-//   return (
-//   <div className='App' >
-//     <h1>Code with useEffect</h1>
-//     <button onClick={increment}>(+) Increment button</button>
-//     <button onClick={decrement}>(-) Decrement button</button>
-//   </div>
-//   );
-// }
-// export default App;
->>>>>>> fe61edb (05 useState useEffect for rendring)
